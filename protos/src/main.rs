85087854719b20bdc2966ee98101c1ad26f3b46b
Philipp Reiter <p.reiter93@gmail.com>
2025-06-30 21:52:41 +0200

    add app and move backend to services

--- protos/src/main.rs
+++ protos/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     tonic_build::configure()
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
-        .out_dir("../backend/users/src/")
+        .out_dir("../services/users/src/")
         .compile(&["./users.proto"], &["./"])
         .expect("Failed to compile users protos");
 }
