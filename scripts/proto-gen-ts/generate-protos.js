8cd4599828a6cf1a8cad0c0a1dd68edcefe899d1
Philipp Reiter <p.reiter93@gmail.com>
2025-07-20 00:05:18 +0200

    generate ts protos for a list of microservices

--- scripts/proto-gen-ts/generate-protos.js
+++ scripts/proto-gen-ts/generate-protos.js
@@ -5,15 +5,18 @@ import { join, resolve, dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// TODO: Do this for all services
-const PROTO_SRC = join(__dirname, "../../services/user/*proto");
-const OUT_DIR = resolve(__dirname, "../../app/src/lib/protos/user");
+const services = ["user", "auth"];
 
-execSync(`proto-loader-gen-types \
-  --longs=String \
-  --keepCase \
-  --defaults \
-  --oneofs \
-  --grpcLib=@grpc/grpc-js \
-  --outDir=${OUT_DIR} \
-  ${PROTO_SRC}`, { stdio: "inherit" });
+services.forEach(service => {
+  const PROTO_SRC = join(__dirname, `../../services/${service}/*proto`);
+  const OUT_DIR = resolve(__dirname, `../../app/src/lib/protos/${service}`);
+
+  execSync(`proto-loader-gen-types \
+    --longs=String \
+    --keepCase \
+    --defaults \
+    --oneofs \
+    --grpcLib=@grpc/grpc-js \
+    --outDir=${OUT_DIR} \
+    ${PROTO_SRC}`, { stdio: "inherit" });
+});
