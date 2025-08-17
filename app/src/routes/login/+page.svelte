f27e16462342c80baf551d38d3afa18323b41cda
Philipp Reiter <p.reiter93@gmail.com>
2025-08-17 16:56:33 +0200

    add github button

--- app/src/routes/login/+page.svelte
+++ app/src/routes/login/+page.svelte
@@ -4,36 +4,57 @@
 	async function loginWithGoogle() {
 		window.location.assign(`${PUBLIC_API_URL}/auth/google/login`);
 	}
+	async function loginWithGithub() {}
 </script>
 
-<div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-	<button class="google-btn" on:click={loginWithGoogle}>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="48"
-			height="48"
-			viewBox="0 0 48 48"
-			class="google-icon"
-			><path
-				fill="#EA4335"
-				d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
-			/><path
-				fill="#4285F4"
-				d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
-			/><path
-				fill="#FBBC05"
-				d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"
-			/><path
-				fill="#34A853"
-				d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
-			/><path fill="none" d="M0 0h48v48H0z" /></svg
-		>
-		Sign in with Google
-	</button>
+<div style="display:flex; justify-content: center; flex-direction: column; height: 100vh;">
+	<div style="display: flex; justify-content: center; align-items: center;">
+		<button class="google btn" on:click={loginWithGoogle}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="48"
+				height="48"
+				viewBox="0 0 48 48"
+				class="google icon"
+				><path
+					fill="#EA4335"
+					d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
+				/><path
+					fill="#4285F4"
+					d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
+				/><path
+					fill="#FBBC05"
+					d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"
+				/><path
+					fill="#34A853"
+					d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
+				/><path fill="none" d="M0 0h48v48H0z" /></svg
+			>
+			Sign in with Google
+		</button>
+	</div>
+
+	<div style=" display: flex; justify-content: center; align-items: center; margin-top: 1rem;">
+		<button class="github btn" on:click={loginWithGithub}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="currentColor"
+				class="github icon"
+			>
+				<path
+					d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.387.6.113.82-.26.82-.577v-2.234c-3.338.726-4.033-1.61-4.033-1.61-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.838 1.237 1.838 1.237 1.07 1.834 2.807 1.304 3.492.997.108-.774.418-1.304.76-1.604-2.665-.303-5.466-1.332-5.466-5.933 0-1.31.468-2.38 1.236-3.22-.124-.303-.536-1.523.117-3.176 0 0 1.008-.322 3.3 1.23a11.52 11.52 0 0 1 3-.404c1.02.005 2.045.137 3 .404 2.29-1.552 3.296-1.23 3.296-1.23.655 1.653.243 2.873.12 3.176.77.84 1.235 1.91 1.235 3.22 0 4.61-2.807 5.625-5.48 5.922.43.37.814 1.102.814 2.222v3.293c0 .32.216.694.825.576C20.565 21.796 24 17.298 24 12c0-6.63-5.37-12-12-12z"
+				/><path fill="none" d="M0 0h24v24H0z" />
+			</svg>
+			Sign in with Github
+		</button>
+	</div>
 </div>
 
 <style>
-	.google-btn {
+	.btn {
 		display: inline-flex;
 		align-items: center;
 		background: white;
@@ -50,11 +71,11 @@
 		box-shadow: 0 1px 2px rgb(0 0 0 / 0.05);
 	}
 
-	.google-btn:hover {
+	.btn:hover {
 		background-color: #f7f7f7;
 	}
 
-	.google-icon {
+	.icon {
 		width: 20px;
 		height: 20px;
 		margin-right: 10px;
